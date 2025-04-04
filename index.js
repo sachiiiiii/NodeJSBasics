78f55d7204ed23d10bde7a148a9240448f881134
Osi <osachi752@gmail.com>
2025-04-04 10:04:11 -0400

    nodemon error fixed

--- index.js
+++ index.js
@@ -36,3 +36,7 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+array.forEach(element => {
+    
+});
